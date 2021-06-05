e768fd8bf481d8a46f9c5eff6a7f5525d929e1bc
dolsotbap <leejeongro1992@gmail.com>
2021-06-05 20:21:47 +0900

    feat: Start TypeScript

---
+++ getting-started/index.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
+//compile: ts파일을 js파일로 변환하는 작업
+//npm i typescript -g: 로컬에 설치
---
+++ getting-started/index.ts
@@ -0,0 +1,9 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+sum(10, 20);
+//compile: ts파일을 js파일로 변환하는 작업
+//npm i typescript -g: 로컬에 설치
+//! 터미널에 "tsc index.ts": 디렉토리에 index.js 생성 -> tsc명령어
+//웹팩같은 모듈 번들러, 그런트, 걸프 등 웹자동화도구 웹 태스크매니저를 통해서 명령어를 자동화 할 수 있게됨
